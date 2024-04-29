a2098aa3897ee6a01eab8ccc0e9326f3385fbd97
KrishnaYamphu <krishnayamphu@gmail.com>
2024-04-29 17:59:45 +0545

    file upload

---
+++ adv/fileupload/index.php
@@ -0,0 +1,58 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $target_dir = "";
+    $str = bin2hex(random_bytes(5)) . "-";
+    $date = date('Y-m-d-');
+    $target_file = $target_dir . $str . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    // Check file size 500kb
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk == 1) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    } else {
+        echo "file can't upload";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload File</title>
+</head>
+
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+</body>
+
+</html>
